f50f5e45c8279620e90d1f0c39cdabf54b81b77b
BrunaMS <br.medeiros@hotmail.com>
2020-07-24 11:55:39 -0300

    - A visual way to understand how the database works; - Queries commands and filters; - Logical operators; - `LIKE` operator; - Using `WHERE <field> IN (<value1>, <value2>, ...)`

--- courses/2_SQL_queries/juices_sales_script.sql
+++ courses/2_SQL_queries/juices_sales_script.sql
@@ -1,3 +1,48 @@
-CREATE DATABASE sucos_vendas; -- I had to use this name because the imported data works only with this database
+CREATE DATABASE IF NOT EXISTS sucos_vendas; -- I had to use this name because the imported data works only with this database
+
+-- Here, import data from the folder "DumpSucosVendas"
+
 USE sucos_vendas; 
-SELECT * FROM itens_notas_fiscais;
+
+-- REVISION ABOUT SELECT COMMAND, QUERIES AND SOME FILTERS
+
+SELECT * FROM tabela_de_clientes;
+
+SELECT CPF, NOME FROM tabela_de_clientes;
+
+SELECT CPF, NOME AS NAME, ENDERECO_1 AS ADDRESS FROM tabela_de_clientes;
+
+SELECT * FROM tabela_de_produtos WHERE CODIGO_DO_PRODUTO > '1005000';
+
+SELECT * FROM tabela_de_produtos WHERE PRECO_DE_LISTA BETWEEN 19 AND 20;
+
+SELECT * FROM tabela_de_produtos WHERE SABOR = 'uva';
+
+SELECT * FROM tabela_de_produtos WHERE SABOR = 'laranja';
+
+-- CONDITIONAL QUERIES
+
+-- LOGICAL OPERATORS
+	-- NOT(EXPRESSION)
+	-- EXPRESSION1 AND EXPRESSION2
+	-- EXPRESSION1 OR EXPRESSION2
+    
+SELECT * FROM tabela_de_produtos WHERE SABOR = 'manga' OR TAMANHO = '470 ml';
+SELECT * FROM tabela_de_produtos WHERE SABOR = 'manga' AND TAMANHO = '470 ml';
+SELECT * FROM tabela_de_produtos WHERE SABOR = 'manga' AND NOT(TAMANHO = '470 ml');
+SELECT * FROM tabela_de_produtos WHERE SABOR = 'manga' OR NOT(TAMANHO = '470 ml');
+SELECT * FROM tabela_de_produtos WHERE SABOR IN ('manga', 'laranja'); -- SABOR = 'laranja' OR SABOR = 'manga';
+SELECT * FROM tabela_de_clientes WHERE CIDADE IN ('Rio de Janeiro', 'São Paulo') AND IDADE <= 22;
+
+-- LIKE OPERATOR 
+	-- ... WHERE <FIELD> LIKE '<CONDITION>'
+	-- % - Any generic record that contains the values/text into <condition>
+		-- At the beginning('<TEXT>%')
+        -- At the end ('%<TEXT>')
+        -- Anywhere ('%<TEXT>%')
+    -- EXAMPLE: LIKE '%Soares%' --> Anyone with 'Soares' into the name
+    
+SELECT * FROM tabela_de_clientes WHERE NOME LIKE '%Silva%';
+SELECT * FROM tabela_de_clientes WHERE NOME LIKE '%a%' AND IDADE > 20; 
+
+SELECT * FROM tabela_de_clientes WHERE NOME LIKE '%Mattos';
